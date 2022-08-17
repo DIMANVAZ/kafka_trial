163837cdeb42cc2696d68b964b6343f95b28ac83
DIMANVAZ <DIMANVAZ04@GMAIL.COM>
2022-08-18 00:48:59 +0300

    started .env actions

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
+//https://www.npmjs.com/package/dotenv
+//https://devcenter.heroku.com/articles/cloudkarafka#node-js
+//https://api.cloudkarafka.com/console/a48ed08f-fb00-4e0e-a5cf-32aef9c1619e/details
+// установлены kafka-node и node-rdkafka
 
-const Chance = require('chance');
+//require('dotenv').config({ debug: true, path: './karafka/.env' });
+require('dotenv').config({ debug: true, path: './karafka/cond/.env' });
+console.log(process.env);
+
+const Chance = require('chance'); //генератор рандомной фигни
 const chance = new Chance();
 
-const kafka = require('kafka-node'),
-    Producer = kafka.Producer,
-    KeyedMessage = kafka.KeyedMessage,
-    client = new kafka.KafkaClient({kafkaHost: '127.0.0.1:9092'}),
-    producer = new Producer(client),
-    km = new KeyedMessage('key', 'message'),
-    payloads = [
-        {topic: 'topec', messages: 'fakafaka', partition: 0 },
-    ];
-producer.on('ready', function () {
-    setInterval(()=>{
-        producer.send([
-            {topic: 'topec', messages: chance.animal(), partition: 0 },
-        ], function (err, data) {
-            console.log(data);
-        });
-    }, 1000)
-
+const Kafka = require("node-rdkafka");
+/*const kafkaConf = {
+    "group.id": "cloudkarafka-example",
+    "metadata.broker.list": process.env.CLOUDKARAFKA_BROKERS.split(","),
+    "socket.keepalive.enable": true,
+    "security.protocol": "SASL_SSL",
+    "sasl.mechanisms": "SCRAM-SHA-256",
+    "sasl.username": process.env.CLOUDKARAFKA_USERNAME,
+    "sasl.password": process.env.CLOUDKARAFKA_PASSWORD
+};
+const prefix = process.env.CLOUDKARAFKA_TOPIC_PREFIX;
+const topic = `${prefix}.test`;
+const producer = new Kafka.Producer(kafkaConf);
+const maxMessages = 20;
+const genMessage = i => new Buffer(`Kafka example, message number ${i}`);
+producer.on("ready", function(arg) {
+    console.log(`producer ${arg.name} ready.`);
+    for (var i = 0; i < maxMessages; i++) {
+        producer.produce(topic, -1, genMessage(i), i);
+    }
+    setTimeout(() => producer.disconnect(), 0);
 });
+producer.on("disconnected", function(arg) {
+    process.exit();
+});
+producer.connect();*/
 
-producer.on('error', function (err) {})
